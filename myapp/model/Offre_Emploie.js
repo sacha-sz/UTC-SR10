038f36044ac6550a27ec6ee67eb254bc1b302148
TobiasInfo <savarytobias@hotmail.com>
2023-04-11 09:10:46 +0200

    test offre emploie en cours

---
+++ myapp/model/Offre_Emploie.js
@@ -0,0 +1,47 @@
+// Créer un fichier par table de la base de données
+
+var db = require('./ConnexionBDD.js');
+
+module.exports = {
+    read: function(numero, callback) {
+        db.query(" SELECT * FROM Fiche_poste WHERE numero= ?;", numero, function(err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    },
+    readall: function(callback) {
+        db.query("SELECT * from Fiche_poste;", function(err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    },
+    // areValid: function(email, password, callback) {
+    //     sql = "SELECT pwd FROM Utilisateur WHERE email = ?";
+    //     rows = db.query(sql, email, function(err, results) {
+    //         if (err) throw err;
+    //         if (rows.length == 1 && rows[0].pwd === password) {
+    //             callback(true)
+    //         } else {
+    //             callback(false);
+    //         }
+    //     });
+    // },
+    create: function(numero, intitule, responsable_hierarchique, lieu_mission_lat, lieu_mission_long, rythme, salaire_min, salaire_max, statut_poste, type_metier, callback) {
+        db.query("INSERT INTO Fiche_poste(numero, intitule, responsable_hierarchique, lieu_mission_lat, lieu_mission_long, rythme, salaire_min, salaire_max, statut_poste, type_metier) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", numero, intitule, responsable_hierarchique, lieu_mission_lat, lieu_mission_long, rythme, salaire_min, salaire_max, statut_poste, type_metier, function(err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    }
+
+    // areValid_login: function(email, password, callback) {
+    //     sql = "SELECT * FROM Utilisateur WHERE email = ? AND password = ?";
+    //     rows = db.query(sql, [email, password], function(err, results) {
+    //         if (err) throw err;
+    //         if (rows.length == 1) {
+    //             callback(true)
+    //         } else {
+    //             callback(false);
+    //         }
+    //     });
+    // }
+}
