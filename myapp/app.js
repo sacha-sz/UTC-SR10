f2244338e676e5c250669cb96f35e493835a3e52
TobiasInfo <savarytobias@hotmail.com>
2023-05-05 14:25:36 +0200

    Inscription entreprise bien avancé  Pas totalement testé => serveur tuxa down

--- myapp/app.js
+++ myapp/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var offreRouter = require('./routes/job_offer');
 var loginRouter = require('./routes/login');
+var EntrepriseRouter = require('./routes/entreprise');
 
 // app.use(express.static('public'));
 
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/job_offer', offreRouter);
 app.use('/login', loginRouter);
+app.use('/entreprise', EntrepriseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ myapp/routes/entreprise.js
@@ -0,0 +1,65 @@
+var express = require('express');
+// var session = require('express-session');
+var router = express.Router();
+var entrepriseModel = require('../model/Entreprise');
+var app = express();
+var path = require('path');
+
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'static')));
+
+/* GET Connexion page. */
+router.get('/', function(req, res, next) {
+    res.render('entreprise_inscription', { title: 'Inscription' });
+    // res.render('connexion', { title: 'Connexion' });
+});
+
+router.post('/inscripion', function(req, res, next) {
+    console.log('Inscription d\'une entreprise');
+    var name = req.body.name;
+    var siren = req.body.SIREN;
+    var lat = req.body.lat;
+    var long = req.body.long;
+    console.log(name);
+    console.log(siren);
+    console.log(lat);
+    console.log(long);
+    if (name == null || siren == null || name == "" || siren == "" || lat == null || lat == "" || long == null || long == "") {
+        console.log("Données manquantes");
+        return res.redirect('/entreprise');
+    }
+    // TODO : to complete
+    if (!TEST_LATITUDE(lat) || !TEST_LONGITUDE(long)) {
+        console.log("Coordonnées GPS invalides");
+        return res.redirect('/entreprise');
+    }
+    // TODO : Vérif siren
+    entrepriseModel.create(siren, name, lat, long, null, function(result) {
+        if (result) {
+            console.log("Entreprise créée");
+            res.redirect('/'); // TODO : redirect to home login/home
+        } else {
+            res.redirect('/entreprise');
+        }
+    });
+})
+
+// Test que la latitude et la longitude soient valides
+function TEST_LATITUDE(latitude) {
+    // Vérification de la latitude
+    // Format : 
+    // - Un nombre décimal compris entre -90 et 90
+    var correct_latitude_test = /^-?([1-8]?\d(\.\d+)?|90(\.0+)?)$/;
+    return correct_latitude_test.test(latitude);
+}
+
+function TEST_LONGITUDE(longitude) {
+    // Vérification de la longitude
+    // Format : 
+    // - Un nombre décimal compris entre -180 et 180
+    var correct_longitude_test = /^-?((1[0-7]|[1-9])?\d(\.\d+)?|180(\.0+)?)$/;
+    return correct_longitude_test.test(longitude);
+}
+
+module.exports = router;
