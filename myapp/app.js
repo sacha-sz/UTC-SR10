674d1ed9bf3ef62a9f4782b7d8fdd567b034839a
TobiasInfo <savarytobias@hotmail.com>
2023-05-09 16:08:58 +0200

    Fix app.js pr 404

--- myapp/app.js
+++ myapp/app.js
@@ -44,24 +44,27 @@ app.use('/entreprise', EntrepriseRouter);
 app.use('/offre', OffreRouter);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//     next(createError(404));
-// });
+app.use(function(req, res, next) {
+   
+    next(createError(404));
+});
 
 
 // // error handler
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    if(res.status(404)){
+        console.log("ICI");
+        res.render('404', { title: "404"});
+    } else {
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    }
 
-app.get("*", function(req, res, next) {
-    res.status(404).render('404', { title: "404"});
 });
 
 
