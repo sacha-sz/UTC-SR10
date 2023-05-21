e0496a72e7e427478c72309022c4915834151f7a
sacha-sz <102785703+sacha-sz@users.noreply.github.com>
2023-05-21 12:43:37 +0200

    Correctif hell

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -16,8 +16,8 @@ app.get('/', function (req, res, next) {
 
 
 app.get('/userslist', function (req, res, next) {
-    result = userModel.readall(function (err,result) {
-        if(err) throw err;
+    result = userModel.readall(function (err, result) {
+        if (err) throw err;
         res.render('userList', {
             title: 'Liste des utilisateurs',
             users: result
@@ -28,7 +28,7 @@ app.get('/userslist', function (req, res, next) {
 app.get('/inscription', function (req, res, next) {
     var ladate = new Date();
     var date_creation = ladate.getFullYear() + "-" + (ladate.getMonth() + 1) + "-" + ladate.getDate();
-    
+
     res.render('inscription', {
         title: "Inscription",
         date: date_creation,
@@ -75,70 +75,68 @@ app.post('/inscription', function (req, res, next) {
     const ddn = req.body.ddn;
     const latitude = req.body.lat;
     const longitude = req.body.long;
-    
+
     // Test que toutes les données soient correctement renseignées
     if (mail == null || password == null || nom == null || prenom == null || tel == null || sexe == null || ddn == null || latitude == null || longitude == null ||
         mail == "" || password == "" || nom == "" || prenom == "" || tel == "" || sexe == "" || ddn == "" || latitude == "" || longitude == "") {
         console.log("Données manquantes");
         req.session.msg = "Données manquantes";
         res.redirect('/users/inscription');
+    } else {
+        // Test que le mail soit valide
+        userModel.TEST_MAIL(mail, function (result) {
+            if (!result) {
+                console.log("Mail au mauvais format");
+                req.session.msg = "Mail au mauvais format";
+                res.redirect('/users/inscription');
+            } else {
+                // Test que le mot de passe soit valide
+                userModel.TEST_MDP(password, function (result) {
+                    if (!result) {
+                        console.log("Mot de passe invalide");
+                        res.redirect('/users/inscription');
+                    } else {
+                        // Test que le numéro de téléphone soit valide
+                        userModel.TEST_TEL(tel, function (result) {
+                            if (!result) {
+                                console.log("Numéro de téléphone invalide");
+                                res.redirect('/users/inscription');
+                            } else {
+                                // Test que la latitude et la longitude soient valides
+                                userModel.TEST_COORDONNEES(latitude, longitude, function (result) {
+                                    if (!result) {
+                                        console.log("Coordonnées GPS invalides");
+                                        res.redirect('/users/inscription');
+                                    } else {
+
+                                        // Test que la date de naissance soit valide
+                                        userModel.TEST_DATE_NAISSANCE(ddn, function (result) {
+                                            if (!result) {
+                                                console.log("Date de naissance invalide");
+                                                res.redirect('/users/inscription');
+                                            } else {
+                                                // utiliser le model pour enregistrer les données récupérées dans la BD
+                                                result = userModel.create(mail, password, nom, prenom, tel, sexe, ddn, latitude, longitude, function (err, result) {
+                                                    if (result == true) {
+                                                        console.log("Utilisateur créé");
+                                                        res.redirect('/');
+                                                    } else {
+                                                        console.log("Utilisateur non créé");
+                                                        res.redirect('/users/inscription');
+                                                    }
+                                                }
+                                                )
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
-    
-    // Test que le mail soit valide
-    userModel.TEST_MAIL(mail, function (result) {
-        if (!result) {
-            console.log("Mail au mauvais format");
-            req.session.msg = "Mail au mauvais format";
-            res.redirect('/users/inscription');
-        }
-    });
-    
-    // Test que le mot de passe soit valide
-    userModel.TEST_MDP(password, function (result) {
-        if (!result) {
-            console.log("Mot de passe invalide");
-            res.redirect('/users/inscription');
-        }
-    });
-    
-    
-    // Test que le numéro de téléphone soit valide
-    userModel.TEST_TEL(tel, function (result) {
-        if (!result) {
-            console.log("Numéro de téléphone invalide");
-            res.redirect('/users/inscription');
-        }
-    });
-    
-    // Test que la latitude et la longitude soient valides
-    userModel.TEST_COORDONNEES(latitude, longitude, function (result) {
-        if (!result) {
-            console.log("Coordonnées GPS invalides");
-            res.redirect('/users/inscription');
-        }
-    });
-    
-    
-    // Test que la date de naissance soit valide
-    userModel.TEST_DATE_NAISSANCE(ddn, function (result) {
-        if (!result) {
-            console.log("Date de naissance invalide");
-            res.redirect('/users/inscription');
-        }
-    });
-    
-    
-    // utiliser le model pour enregistrer les données récupérées dans la BD
-    result = userModel.create(mail, password, nom, prenom, tel, sexe, ddn, latitude, longitude, function (err, result) {
-        if (result == true) {
-            console.log("Utilisateur créé");
-            res.redirect('/'); 
-        } else {
-            console.log("Utilisateur non créé");
-            res.redirect('/users/inscription');
-        }
-    }
-    )
 });
 
 app.post('/changed_infos', function (req, res, next) {
@@ -232,7 +230,7 @@ app.post('/delete_user', function (req, res, next) {
     confirmation = confirmation.toUpperCase();
     console.log(confirmation);
     console.log("dans delete_user POST");
-    
+
     if (confirmation == "CONFIRMER") {
         console.log("Suppression de l'utilisateur");
         userModel.delete(req.session.username, function (err, result) {
