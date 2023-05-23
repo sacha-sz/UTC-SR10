90fb4fcaf2fe752d54df569e8c81dc7c8d0dc79c
TobiasInfo <savarytobias@hotmail.com>
2023-05-23 10:36:09 +0200

    Gestion entreprise admin ok

--- myapp/routes/admin.js
+++ myapp/routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var userModel = require('../model/Utilisateur');
 var adminModel = require('../model/Admin');
+var entrepriseModel = require('../model/Entreprise');
 var app = express();
 var path = require('path');
 
@@ -30,6 +31,27 @@ app.get('/gestion_utilisateur', function (req, res, next) {
     }
 });
 
+app.get('/gestion_entreprise', function (req, res, next) {
+    if (req.session.loggedin) {
+        entrepriseModel.readall(function (err, result) {
+            if (err) {
+                // Gérer l'erreur si nécessaire
+                console.log(err);
+                res.status(500).send("Une erreur s'est produite");
+            } else {
+                res.render('AdminGestionEntreprise', {
+                    title: 'Liste des Entreprises',
+                    username: req.session.username,
+                    type_user: req.session.type_user,
+                    entreprises: result
+                });
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
+
 app.post('/passer_admin', function (req, res, next) {
     adminModel.updateTypeUtilisateur(req.body.email, "ADMINISTRATEUR", function (err, result) {
         if (err) {
@@ -42,10 +64,25 @@ app.post('/passer_admin', function (req, res, next) {
     });
 });
 
+
+
 app.post('/passer_recruteur', function (req, res, next) {
     // TODO : passer le type de l'utilisateur à "RECRUTEUR"
     // Demander le siren de l'entreprise
     // Peut être faire une branchement vers entreprise ?
+});
+
+
+app.post('/delete_entreprise', function (req, res, next) {
+    entrepriseModel.delete(req.body.siren, function (err, result) {
+        if (err) {
+            // Gérer l'erreur si nécessaire
+            console.log(err);
+            throw err;
+        } else {
+            res.redirect('/admin/gestion_entreprise');
+        }
+    });
 });
 
 module.exports = app;
